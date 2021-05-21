728954d311fa942c67ef698393f0ed31e98a0d0c
psytron <m@psytron.com>
2021-05-20 16:06:42 -1000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,4 @@
-ray( wrapper ) 
-import ray
+import space
 
 
-ray.init( '0.0.0.0' )
-
-PoolModel = ray.remote( PoolModel )
-poolModel = PoolModel.options(name='modelactor').remote()  #, lifetime='detached'
-poolModel.run.remote()
-
-cs = [ Tokens, Pool ]
-
-# process_objects 
-
-
-def run( process_objects ):
-    wrapped = [ ray.remote( c ) for c in process_objects ]
-    procs = [ p.remote() for p in wrapped ]
-    [ x.run.remote() for x in procs ]
+run = space.run
